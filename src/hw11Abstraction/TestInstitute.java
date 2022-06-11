b698738995f8cc52dd6005c179ba81ce2f341b6b
Tenzin Nyandak <tenzin_ny@Tenzins-MacBook-Pro.local>
2022-06-11 19:02:16 -0400

    06112022 7:00pm

--- src/hw11Abstraction/TestInstitute.java
+++ src/hw11Abstraction/TestInstitute.java
@@ -1,12 +1,5 @@
 package hw11Abstraction;
 
-/*Now create a class TestInstitute. Then Instantiate  ColumbiaUniversity , University and MedicalSchool
- *one by one. Call all the possible methods from the object of ColumbiaUniversity , University and 
- *MedicalSchool. Make sure you execute the static methods from ColumbiaUniversity , University and 
- *MedicalSchool (if not created, please create one). Try to create default method (public default 
- *void ...........[follow the format of default method from interface]) in regular class and abstract 
- *class if possible and execute them if possible. 
- */
 public class TestInstitute {
 	public static void main(String[] args) {
 		
